fa31ae4797c1eaeb8354fc255a9668d0cc8c59db
newtonrodrigues <newton.nw100@gmail.com>
2022-01-23 01:40:41 -0300

    Updating Commit

--- javascript.js
+++ javascript.js
@@ -32,14 +32,14 @@
          num1 = document.getElementById("num1").value
          num2 = document.getElementById("num2").value
          res = parseInt(num1) + parseInt(num2)
-         document.getElementById("resultado").value = res
+         document.getElementById("resultado").innerText = res
     }
 
     function subtracao(){
         num1 = document.getElementById("num1").value
         num2 = document.getElementById("num2").value
         res  = parseInt(num1) - parseInt(num2)
-        document.getElementById("resultado").value = res
+        document.getElementById("resultado").innerText = res
         console.log(res)
     }
     
@@ -47,7 +47,7 @@
         num1 = document.getElementById("num1").value
         num2 = document.getElementById("num2").value
         res = parseInt(num1) / parseInt(num2)
-        document.getElementById("resultado").value = res
+        document.getElementById("resultado").innerText = res
         alert(res)
     }
 
@@ -55,8 +55,94 @@
         num1 = document.getElementById("num1").value
         num2 = document.getElementById("num2").value
         res = parseInt(num1) * parseInt(num2)
-        document.getElementById("resultado").value = res
+        document.getElementById("resultado").innerText = res
         alert(res)
         console.log(res)
+    }
+
+    
+    var numero = 0
+    
+    function clique(celula){
+       // alert("Oi")
+       // alert(Math.floor(Math.random() * 4));
+       //randomico()
+      // alert(numero)
+   if (numero == celula) {
+      while(numero == celula){
+          randomico()
+      }
+
+      document.getElementById(String(numero)).innerText = "Clique Aqui"
+      document.getElementById(String(celula)).innerText = ''
+
+     // alert(numero)
+    
+    } else {
+        exibirMensagem()
+    }
+}
+
+function exibirMensagem(){
+    frases = [
+        'Bah!',
+        'Jezuiz!',
+        'Vish',
+        'Noss Sinhora!',
+        'Cristo',
+        'Oh Maluco!',
+    ]
+    numerorandom = Math.floor(Math.random() * frases.length)
+    alert(frases[numerorandom])
+}
+
+    function randomico(){
+        numero = Math.floor(Math.random() * 4)
+
+    }
+
+    function clique2(){
+        alert("Oi2")
+    }
+
+    function clique3(){
+        alert("Oi3")
+    }
+
+    function clique4() {
+        alert("Oi4")
+    }
+
+    function imprimir() {
+        var num1 = "1.5"
+        var num2 = "2"
+        alert(document.getElementById("resultado").innerText)
+        alert(parseFloat(num1) + parseFloat(num2))
+    }
+
+    function exibirTime(){
+        
+        selecao = document.getElementById("time").value
+        //alert(selecao)
+        switch (selecao) {
+            case "1":
+                console.log("Seu time é o Bahia")
+                    break;
+            case "2":
+                console.log("São Paulo") 
+                    break;
+            case "3":
+                console.log("Flamengo!")
+                    break;
+            case "4":
+                console.log("Cruzeiro!")
+                alert("Cruzeiro!")
+                    break;
+            default:
+                console.log("Não tem time")
+                break;
+        }
+       
+
     }
 
